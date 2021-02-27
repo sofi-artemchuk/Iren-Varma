7f2a29753113669cd5bbfb73efe9f1b584feff2d
Sofia Artemchuk <sofi.artemchuk@gmail.com>
2021-02-27 16:12:55 +0000

    carousel

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
 var slideIndex = 1;
-showSlides(1);
+showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slides_item");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+  if (n > slides.length-1) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
@@ -22,5 +18,7 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-0].style.display = "block";
   dots[slideIndex-1].className += " active";
+  
 }
